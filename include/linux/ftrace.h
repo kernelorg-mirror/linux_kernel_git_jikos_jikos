a2c39141936ffcff18be1e37ff0a804d7632e300
Jiri Slaby <jslaby@suse.cz>
2014-04-22 19:47:11 +0200

    ftrace: Add function to find fentry of function
    This is needed for kgr to find fentry location to be "ftraced". We use
    this to find a place where to jump to a new/old code location.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -299,6 +299,7 @@ extern void
 unregister_ftrace_function_probe_func(char *glob, struct ftrace_probe_ops *ops);
 extern void unregister_ftrace_function_probe_all(char *glob);
 
+extern unsigned long ftrace_function_to_fentry(unsigned long addr);
 extern int ftrace_text_reserved(const void *start, const void *end);
 
 extern int ftrace_nr_registered_ops(void);
